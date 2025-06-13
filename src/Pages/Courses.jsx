a746e48e6f19809ab93bd88190131729a0dbc6fd
ramimahammadarpon <ramim6708@gmail.com>
2025-06-13 12:02:28 +0600

    completed courses page

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../Components/card';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(()=> {
+        fetch('http://localhost:3000/courses').then(res=> res.json()).then(data=> setCourses(data));
+    }, [])
     return (
-        <div>
-            This is Courses Page
+        <div className="relative bg-gradient-to-b from-secondary/40 to-primary/60 min-h-screen py-20 lg:py-30">
+            <h1 className='text-center font-semibold text-2xl text-primary mb-7'>Courses</h1>
+
+            <div className='grid grid-cols-3 gap-3 max-w-7xl mx-auto'>
+                {
+                    courses.map((course, index) => <Card key={index} course={course} ></Card>)
+                }
+            </div>
         </div>
     );
 };
